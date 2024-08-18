736e6d879f7923b271b0e01608eb92f69079b061
OluwadaraJ <oluwadarajolaade@gmail.com>
2024-08-18 20:38:21 +0100

    major commit

---
+++ message.js
@@ -0,0 +1,20 @@
+// sarcasticMessageGenerator.js
+
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateSarcasticMessage() {
+    const subjects = ["Oh, you’re", "Wow, you’ve really", "Amazing, you’ve"];
+    const verbs = ["solved all the world's problems", "become a coding genius", "revolutionized JavaScript"];
+    const endings = ["...in your dreams.", "by sitting there.", "by doing absolutely nothing."];
+    
+    const subject = getRandomElement(subjects);
+    const verb = getRandomElement(verbs);
+    const ending = getRandomElement(endings);
+
+    return `${subject} ${verb} ${ending}`;
+}
+
+console.log(generateSarcasticMessage());
+
